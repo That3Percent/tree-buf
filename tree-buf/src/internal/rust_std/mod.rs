ed95f14c93684467b04c2702e33f134a0ed0e3d2
Zac Burns <That3Percent@gmail.com>
2020-02-09 09:29:43 -0600

    Organized rust_std

---
+++ tree-buf/src/internal/rust_std/mod.rs
@@ -0,0 +1,5 @@
+mod smart_pointers;
+mod usize;
+
+pub use smart_pointers::*;
+pub use self::usize::*;
--- tree-buf/src/internal/rust_std/smart_pointers.rs
+++ tree-buf/src/internal/rust_std/smart_pointers.rs
@@ -1,7 +1,4 @@
 use crate::prelude::*;
-#[cfg(feature = "read")]
-use crate::internal::encodings::varint::decode_prefix_varint;
-use std::vec::IntoIter;
 
 // TODO: impl Writable for () {
 #[cfg(feature = "write")]
@@ -52,36 +49,3 @@ impl<T: ReaderArray> ReaderArray for BoxReaderArray<T> {
         Ok(Box::new(self.inner.read_next()?))
     }
 }
-
-
-
-#[cfg(feature = "read")]
-impl ReaderArray for IntoIter<usize> {
-    type Read = usize;
-    fn new(sticks: DynArrayBranch<'_>) -> ReadResult<Self> {
-        todo!();
-    }
-    fn read_next(&mut self) -> ReadResult<Self::Read> {
-        self.next().ok_or_else(|| ReadError::InvalidFormat(InvalidFormat::ShortArray))
-    }
-}
-
-
-// TODO: Come back to usize
-// TODO: Error check that the result fits in the platform size
-#[cfg(feature = "read")]
-pub fn read_usize(bytes: &[u8], offset: &mut usize) -> ReadResult<usize> {
-    Ok(decode_prefix_varint(bytes, offset)? as usize)
-}
-
-/*
-
-impl Readable for usize {
-    type ReaderArray = IntoIter<usize>;
-    fn read(sticks: DynRootBranch<'_>) -> ReadResult<Self> {
-        Ok(u64::read(sticks)? as Self)
-    }
-}
-
-*/
-
